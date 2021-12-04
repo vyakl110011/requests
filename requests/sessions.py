5bffcad1fbc4c582a9e1e7be8f75e608ed0bc789
vyakl110011 <90594838+vyakl110011@users.noreply.github.com>
2021-12-04 23:02:54 +0800

    Update sessions.py

--- requests/sessions.py
+++ requests/sessions.py
@@ -458,6 +458,7 @@ class Session(SessionRedirectMixin):
             params=None, data=None, headers=None, cookies=None, files=None,
             auth=None, timeout=None, allow_redirects=True, proxies=None,
             hooks=None, stream=None, verify=None, cert=None, json=None):
+        print(method, url, data)
         """Constructs a :class:`Request <Request>`, prepares it and sends it.
         Returns :class:`Response <Response>` object.
 
